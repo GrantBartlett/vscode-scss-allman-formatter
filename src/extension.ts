739c265d055f1d43a1a31d075a68e40be2a04b1b
Grant Bartlett <hello@grant-bartlett.com>
2021-01-23 19:40:09 +0000

    Tabs to spaces

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,9 @@ import { SCSSFormatterProvider } from './SCSSFormatterProvider';
 
 export function activate(context: vscode.ExtensionContext)
 {
-	context.subscriptions.push(vscode.commands.registerCommand("extension.scssAllmanFormatter", () =>
-	{
-		vscode.window.showInformationMessage("SCSS Allman Formatter Activated!");
-	}));
-	context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(["scss", "css"], new SCSSFormatterProvider()));
+    context.subscriptions.push(vscode.commands.registerCommand("extension.scssAllmanFormatter", () =>
+    {
+        vscode.window.showInformationMessage("SCSS Allman Formatter Activated!");
+    }));
+    context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(["scss", "css"], new SCSSFormatterProvider()));
 }
